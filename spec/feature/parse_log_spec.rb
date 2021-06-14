d7b96860081f591fe9d1cb0da6349a8b23ff4648
lessless <yevhenii.kurtov@gmail.com>
2021-06-15 01:28:28 +0300

    Remove extension from script file

--- spec/feature/parse_log_spec.rb
+++ spec/feature/parse_log_spec.rb
@@ -4,7 +4,7 @@ describe "Parsing a log file" do
   let(:logfile) { "spec/fixtures/webserver.log" }
 
   it "outputs amount of page visits" do
-    expectation = expect { system("./bin/parser.rb #{logfile}") }.to output(<<~TABLE).to_stdout_from_any_process
+    expectation = expect { system("./bin/parser #{logfile}") }.to output(<<~TABLE).to_stdout_from_any_process
       Page  | Number of visits
       /home | 3 visits
       /index| 2 visits
