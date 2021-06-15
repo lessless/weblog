08eba1fcc7605decaff34458f4ab2f2d2a34e7e8
lessless <yevhenii.kurtov@gmail.com>
2021-06-16 00:02:41 +0300

    Parse log lines in Parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -6,10 +6,14 @@ class Parser
     @reader = reader
     @repo = repository
     @printer = printer
+    @log_entry = Parser::LogEntry.new
   end
 
   def run
-    reader.stream { |path, ip| repo.add_visit(path, ip) }
+    reader.stream do |log_line|
+      parsed_entry = @log_entry.from(log_line)
+      repo.add_visit(parsed_entry[:path], parsed_entry[:ip]) if @log_entry.valid?(parsed_entry)
+    end
     printer.table(Parser::Visits.new(repo.visits))
     print "\n"
     printer.table(Parser::Views.new(repo.views))
--- lib/parser/log_file_reader.rb
+++ lib/parser/log_file_reader.rb
@@ -5,18 +5,17 @@ class Parser
   class LogFileReader
     def initialize(logfile)
       @logfile = logfile
-      @log_entry = Parser::LogEntry.new
     end
 
     def stream
       File.open(logfile, 'r').each_line do |log_line|
-        parsed_entry = @log_entry.from(log_line)
-        yield(parsed_entry[:path], parsed_entry[:ip]) if @log_entry.valid?(parsed_entry)
+        log_line = log_line.chop
+        yield(log_line) unless log_line.empty?
       end
     end
 
     private
 
-    attr_reader :logfile, :log_entry
+    attr_reader :logfile
   end
 end
