77863cdc262785f762e58cb0e0ccefd47104435b
lessless <yevhenii.kurtov@gmail.com>
2021-06-16 10:37:52 +0300

    Skip entries with invalid IPs

--- spec/lib/parser/log_entry_spec.rb
+++ spec/lib/parser/log_entry_spec.rb
@@ -13,17 +13,17 @@ describe 'Parser::LogEntry' do
     end
 
     it 'can handle empty string' do
-      expect(subject.from('')).to eql({ path: '', ip: 'Invalid IP' })
-    end
-
-    it 'replaces malformed IP with `Invalid IP`' do
-      expect(subject.from('/home 256.255.255.255')).to eql({ path: '/home', ip: 'Invalid IP' })
+      expect(subject.from('')).to eql({ path: '', ip: '' })
     end
   end
 
   context '#valid?' do
     it 'returns false if path is empty' do
       expect(subject.valid?({ path: '', ip: '1.1.1.1' })).to be_falsy
+    end
+
+    it 'returns false if IP is invalid`' do
+      expect(subject.valid?({ path: '/home', ip: '256.1.1.1'})).to be_falsy
     end
   end
 end
