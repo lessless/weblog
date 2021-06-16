8a6a95bd7aab835220764522fb7068eba99cbf7a
lessless <yevhenii.kurtov@gmail.com>
2021-06-16 22:28:37 +0300

    Improve wording in LogFileReader spec

--- spec/lib/parser/log_file_reader_spec.rb
+++ spec/lib/parser/log_file_reader_spec.rb
@@ -7,22 +7,22 @@ require 'parser/log_entry'
 describe 'Parser::LogFileReader' do
   subject { Parser::LogFileReader.new(logfile) }
 
-  context 'when parsing a file' do
+  context 'when reading a file' do
     let(:logfile) { 'spec/fixtures/minimal.log' }
 
-    it 'passes log file entries to the injected object line by line' do
+    it 'passes log file entries to the collaborator line by line' do
       collector = []
 
       subject.stream { |path, ip| collector << { path: path, ip: ip } }
 
       expect(collector).to eql([{ path: '/home', ip: '1.1.1.1' }])
     end
-  end
 
-  context 'when parsing a file with empty lines at the end' do
+
+  context 'when reading a file with empty lines at the end' do
     let(:logfile) { 'spec/fixtures/minimal_with_empty_line.log' }
 
-    it 'skips them' do
+    it 'skips empty line at the end of file' do
       collector = []
 
       subject.stream { |path, ip| collector << { path: path, ip: ip } }
